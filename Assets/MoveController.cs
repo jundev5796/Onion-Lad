9642507cc8c2d2e6ad58dd158f2c529cfb73d68c
HyunJun Baek <hjbaek5796@gmail.com>
2024-11-27 14:44:35 +0900

    implement jump function

--- Assets/MoveController.cs
+++ Assets/MoveController.cs
@@ -5,6 +5,7 @@ public class MoveController : MonoBehaviour
 
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
     private float xInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +18,22 @@ public class MoveController : MonoBehaviour
     void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+    }
+
+    private void Movement()
+    {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
     }
 }
