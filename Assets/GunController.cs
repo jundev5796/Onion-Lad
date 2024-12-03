8d595c90965e463e5fc5450e9a9bc61c798bca58
HyunJun Baek <hjbaek5796@gmail.com>
2024-12-03 15:31:29 +0900

    implement gun aim functionality

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,15 +6,19 @@ public class GunController : MonoBehaviour
 {
     [SerializeField] private Animator gunAnim;
     [SerializeField] private Transform gun;
+    [SerializeField] private float gunDistance = 1.5f;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - gun.position;
+        Vector3 direction = mousePos - transform.position;
 
         gun.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg));
-    
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        gun.position = transform.position + Quaternion.Euler(0, 0, angle) * new Vector3(gunDistance, 0, 0);
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
             Shoot();
     }
