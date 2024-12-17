e57f3f1ce27b7a9d29091ab333e83974fe31a613
HyunJun Baek <hjbaek5796@gmail.com>
2024-12-17 18:04:48 +0900

    [Bullet] destroy target when bullet collides

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,4 +5,13 @@ public class Bullet : MonoBehaviour
     private Rigidbody2D rb => GetComponent<Rigidbody2D>();
 
     void Update() => transform.right = rb.linearVelocity;
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if(collision.tag == "Target")
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
 }
